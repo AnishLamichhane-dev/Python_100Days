5ecbfab68a457787feed181e10f5f4192951733b
AnishLamichhane-dev <anishcryptic@gmail.com>
2025-10-25 16:59:08 +0545

    Python_Day_001_Band_Name_Generator

---
+++ Day_001_Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator.")
+city_name = input("Enter your city's name: \n")
+pet_name = input("Enter your pet's name: \n")
+band_name = city_name + " " + pet_name
+print("Your band name could be" + " " + band_name)
