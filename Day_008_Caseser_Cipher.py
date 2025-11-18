84ee19ae440b9c6ad5affdce0c99090296be7319
AnishLamichhane-dev <anishcryptic@gmail.com>
2025-11-18 12:42:59 +0545

    Caseser Cipher

---
+++ Day_008_Caseser_Cipher.py
@@ -0,0 +1,60 @@
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+end = False
+print(logo)
+while end == False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    def caeser(t, s, d):
+        end_text = ""
+        for char in t:
+            if char in alphabet:
+                position = alphabet.index(char)
+                if d == "encode":
+                    encrypted_position = position + s
+                    encrypted_char = alphabet[encrypted_position]
+                    end_text += encrypted_char
+
+                elif d == "decode":
+                    decrypted_position = position - s
+                    decrypted_char = alphabet[decrypted_position]
+                    end_text += decrypted_char
+            else:
+                end_text += char
+
+        print(f"The {d}d text is {end_text}")
+
+    caeser(t=text, s=shift, d=direction)
+    once_more = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'.\n >>>").lower()
+    if once_more == "no":
+        end = True
+        print("Bye!")
+    elif once_more == "yes":
+        end = False
+
