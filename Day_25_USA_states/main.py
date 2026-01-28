44f1290a0d840529480abfdb6df0679088f7f51c
AnishLamichhane-dev <anishcryptic@gmail.com>
2026-01-29 02:31:39 +0545

    name all states

---
+++ Day_25_USA_states/main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+states_data = pandas.read_csv("50_states.csv")
+states_name_list =states_data.state.tolist()
+
+guessed_states  = []
+score = 0
+
+while len(guessed_states) < 50:
+    user_answer = screen.textinput(f"{len(guessed_states)}/50 States Correct","What's another state's name?").title()
+
+    if user_answer == "Exit":
+        missing_states = []
+        for each_state in states_name_list:
+            if each_state not in guessed_states:
+                missing_states.append(each_state)
+                overall=pandas.DataFrame(missing_states)
+                overall.to_csv("states_to_learn.csv") 
+        break
+
+    if user_answer in states_name_list:
+        guessed_states.append(user_answer)
+        score+=1
+        correct_state = states_data[states_data.state == user_answer]
+
+        tim = turtle.Turtle()
+        tim.ht()
+        tim.teleport((correct_state["x"]).tolist()[0],(correct_state["y"]).tolist()[0])
+        tim.write(f"{user_answer}", move=False, align='center', font=('Arial', 6, 'normal'))
+
+screen.exitonclick()
+
+
